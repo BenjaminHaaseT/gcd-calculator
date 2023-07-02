6cfbb1237a09beb90c3da1ad385fb10ace2b76c3
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-01 18:54:07 -0700

    Got gcd functionality working fine

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const inputA = document.getElementById("integer-a");
-const inputB = document.getElementById("integer-b");
+const inputA = document.getElementById('integer-a');
+const inputB = document.getElementById('integer-b');
+const gcdOutput = document.getElementById('gcd-output');
+const gcdBtn = document.getElementById('gcd-btn');
 
 
 function gcd(a, b) {
+    if (a === 0 && b === 0) {
+        alert('gcd(a, b) is undefined whenever a = 0 and b = 0');
+        return;
+    }
     let r = a % b;
     while (r > 0) {
         a = b;
@@ -12,4 +18,28 @@ function gcd(a, b) {
     return b;
 }
 
+function lcm(a, b) {
+    if (a === 0 && b === 0) {
+        alert('lcm(a, b) is undefined whenever a = 0 and b = 0');
+    }
+    return (a * b) / gcd(a, b);
+}
+
+function gcdClickHandler() {
+    let a = parseInt(inputA.value);
+    let b = parseInt(inputB.value);
+    // Check to make sure we have valid integer values for both inputs
+    if (!a || !b) {
+        return;
+    }
+    let d = gcd(a, b);
+    renderGcdOutput(d);
+}
+
+function renderGcdOutput(d) {
+    gcdOutput.textContent = `${d}`;
+    gcdOutput.classList.toggle('invisible');
+}
+
+gcdBtn.addEventListener('click', gcdClickHandler);
 
