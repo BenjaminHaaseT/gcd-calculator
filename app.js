9b2447968b5cbb1c24b6833d1f201da41a9655fe
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-02 17:55:00 -0700

    Appears to be working just fine for all practical purposes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const inputA = document.getElementById('integer-a');
 const inputB = document.getElementById('integer-b');
-const gcdOutput = document.getElementById('gcd-output');
 const gcdBtn = document.getElementById('gcd-btn');
-
+const gcdOutput = document.getElementById('gcd-output');
+const lcmBtn = document.getElementById('lcm-btn');
+const lcmOutput = document.getElementById('lcm-output');
 
 function gcd(a, b) {
     if (a === 0 && b === 0) {
@@ -30,16 +31,52 @@ function gcdClickHandler() {
     let b = parseInt(inputB.value);
     // Check to make sure we have valid integer values for both inputs
     if (!a || !b) {
+        console.log("error, invalid input");
         return;
     }
     let d = gcd(a, b);
     renderGcdOutput(d);
 }
 
-function renderGcdOutput(d) {
-    gcdOutput.textContent = `${d}`;
+function lcmClickHandler() {
+    let a = parseInt(inputA.value);
+    let b = parseInt(inputB.value);
+    if (!a || !b) {
+        console.log("error, invalid input");
+        return;
+    }
+    let l = lcm(a, b);
+    renderLcmOutput(l);
+}
+
+function toggleGcdOutput() {
     gcdOutput.classList.toggle('invisible');
 }
 
+function toggleLcmOutput() {
+    lcmOutput.classList.toggle('invisible');
+}
+
+function toggleOutput() {
+    toggleGcdOutput();
+    toggleLcmOutput();
+}
+
+function renderGcdOutput(d) {
+    gcdOutput.textContent = `${d}`;
+    toggleGcdOutput();
+}
+
+function renderLcmOutput(l) {
+    lcmOutput.textContent = `${l}`;
+    toggleLcmOutput();
+}
+
+
+
+inputA.addEventListener('focus', toggleOutput);
+inputB.addEventListener('focus', toggleOutput);
 gcdBtn.addEventListener('click', gcdClickHandler);
+lcmBtn.addEventListener('click', lcmClickHandler);
+
 
